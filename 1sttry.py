42cb2cae894b9a595c9ae6d10c361286bdce583f
bara2ik <bara25ik@gmail.com>
2025-09-01 13:09:08 +0300

    game rock paper scissors

--- 1sttry.py
+++ 1sttry.py
@@ -2,27 +2,47 @@ import random
 
 
 options = ['rock', 'paper', 'scissors']
-computer = random.choice(options)
 
-player = input("enter your name: ")
-choice = input("enter : rock, paper, scissors: ")
+def get_choice():
+    
+    player1 = input("enter the player's name: ")
+    player2 = input("enter the 2ND player's name: ")
+    while True:
+        entry = input("enter  : rock, paper, scissors: ")
+        entry1 = input("enter : rock, paper, scissors: ")
+        choices = {'player1': entry , 'player2' : entry1}
+        if entry  not in options or  entry1 not in options:
+            print("Invalid choice! Please enter rock, paper, or scissors.")
+            continue
+        choices = {'player1': entry , 'player2' : entry1}
+        return choices
 
 
 
 
 
-if choice not in options:
-    print("Invalid choice! Please enter rock, paper, or scissors.")
-elif choice == computer:
-    print("tie")
-elif choice == 'rock' and computer == 'scissors':
-     print(player + ' wins')
-elif choice == 'paper' and computer == 'rock':
-    print(player + ' wins')     
-elif choice == 'scissors' and computer == 'paper':
-    print(player + ' wins') 
-else:
-    print("computer wins")  
 
-print("computer chose: " + computer)
+def getresult(choice1 ,choice2):
+   
+        
+    if choice1 == choice2:
+        print("tie")
+    elif choice1 == 'rock' and choice2 == 'scissors':
+        print('player1 wins')
+    elif choice1 == 'paper' and choice2 == 'rock':
+        print(' player1 wins')     
+    elif choice1 == 'scissors' and choice2 == 'paper':
+        print('player1 wins') 
+    else:
+        print("player2 wins") 
+    
+ 
+
+
+
+choice = get_choice()
+
+print(getresult(choice['player1'], choice['player2']))
+
+
 
