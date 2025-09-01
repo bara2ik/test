6f94f24c5b8e434a867a5f50c2e57e83417a5e65
bara2ik <bara25ik@gmail.com>
2025-09-01 11:20:22 +0300

    py2

--- 1sttry.py
+++ 1sttry.py
@@ -1,3 +1,28 @@
-one = 1 
-if one =1 : 
-    print("hello world")
+import random
+
+
+options = ['rock', 'paper', 'scissors']
+computer = random.choice(options)
+
+player = input("enter your name: ")
+choice = input("enter : rock, paper, scissors: ")
+
+
+
+
+
+if choice not in options:
+    print("Invalid choice! Please enter rock, paper, or scissors.")
+elif choice == computer:
+    print("tie")
+elif choice == 'rock' and computer == 'scissors':
+     print(player + ' wins')
+elif choice == 'paper' and computer == 'rock':
+    print(player + ' wins')     
+elif choice == 'scissors' and computer == 'paper':
+    print(player + ' wins') 
+else:
+    print("computer wins")  
+
+print("computer chose: " + computer)
+
